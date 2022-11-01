0b014e72e0b4bec9ccad17be228b7d8734979f0d
Maxi <mcortes.na@gmail.com>
2022-11-01 08:22:30 -0300

    restaurar repositorio post contenido inicial Mariela

---
+++ dist/js/scripts/video-player.js
@@ -0,0 +1,39 @@
+// TODO: Implementar esto como clase/dependencia AudioPlayer.
+// Registrar componentes en App.DOMComponents
+
+//Los nombres de las funciones tienen que ser exactamente estos!
+//Y siempre tienen que devolver promesas
+
+// const toggleClass = (element, className) => {
+//     if(element.classList.contains(className)) {
+//         element.classList.remove(className);
+//     }
+//     else {
+//         element.classList.add(className);
+//     }
+// };
+
+const afterInsert = () => {
+    return new Promise( (resolve) => {
+        
+        const videoPlayer = document.querySelector('.video-player');
+        // const playPauseOverlay = document.querySelector('.video-control[data-video-player-behavior="play-pause"]');
+
+        videoPlayer.style.opacity = 0.5;
+        videoPlayer.style.transition = '0.4s';
+
+        videoPlayer.addEventListener('click', () => {
+            if (videoPlayer.paused) {
+                videoPlayer.style.opacity = 1;
+                videoPlayer.play();
+            }
+            else {
+                videoPlayer.pause();
+                videoPlayer.style.opacity = 0.5;
+            }
+        });
+        resolve();
+    } );
+}
+
+export default { afterInsert };

26c3cef09c09afc2c6fed12575f0703f5fdd5f55
Maxi <mcortes.na@gmail.com>
2022-11-16 17:03:22 -0300

    rompecabezas

--- dist/js/scripts/draggable.js
+++ dist/js/scripts/draggable.js
@@ -1,12 +1,17 @@
-// -- Module variables
+// ---- Module variables ----
 
 let draggableElements = [];
+let draggingContext = '';
+const draggableClass = 'draggable';
 const draggingClass = 'dragging';
 const draggingContextClass = 'dragging-context';
 
 
 
-// Event handling (for both desktop and mobile)
+
+// ---- Event handling ----
+
+// Variables and functions to handle events in both desktop and mobile
 
 const eventMap = {
     'enter': ['mousedown', 'touchstart'],
@@ -30,37 +35,71 @@ const removeListener = (element, type, callback) => {
     }
 }
 
-const releasedDragEvent = (event) => eventMap.release.includes(event.type);
+//Check for event types
+const eventIsReleasingDrag = (event) => eventMap.release.includes(event.type);
 
-const enteredDragEvent = (event) => eventMap.enter.includes(event.type);
-
-const draggingEvent = (event) => eventMap.move.includes(event.type);
-
+//Get target in both desktop and mobile
 const getTarget = (event) => event.touches ? event.touches[0].target : event.target;
 
-// -- Module functions
 
-const isDragging = (element) => {
-    return element.classList.contains(draggingClass);
-}
+
+
+// ---- Module functions ----
+
+const elementIsDragging = (element) => element.classList.contains(draggingClass);
+
+const mouseIsInsideDraggingContext = (event) => event.path.includes(draggingContext);
 
 const toggleDraggingState = (event) => {
     event.preventDefault();
     const target = getTarget(event);
-    if ( releasedDragEvent(event) ) {
+    
+    if ( eventIsReleasingDrag(event) ) {
         target.classList.remove(draggingClass);
         return;
     }
     target.classList.add(draggingClass);
 }
 
+const abortAllDraggingMotion = () => {
+    //Remove "dragging" class from any dragged element
+    const draggedElements = document.querySelectorAll(`.${draggingClass}`);
+    for (let element of draggedElements) {
+        element.classList.remove(draggingClass);
+    }
+}
+
+const initPosition = (element) => {
+    if (!element.dataset.xPosition) { element.dataset.xPosition = 0; }
+    if (!element.dataset.yPosition) { element.dataset.yPosition = 0; }
+}
+
+const repositionElement = (element) => {
+    initPosition(element);
+    element.style.transform = `translate(${element.dataset.xPosition}px, ${element.dataset.yPosition}px)`;
+}
+
 const drag = (event) => {
     event.preventDefault();
     const target = getTarget(event);
-    if (isDragging(target)) {
-        console.log('now dragging');
+    //early returns
+    if (!elementIsDragging(target)) {
+        target.style.zIndex = 0;
+        return;
     }
+    if (!mouseIsInsideDraggingContext(event)) { return; }
+    //Drag
+    target.style.zIndex = 10000;
+    initPosition(target);
+    target.dataset.xPosition = parseInt(target.dataset.xPosition) + event.movementX;
+    target.dataset.yPosition = parseInt(target.dataset.yPosition) + event.movementY;
+    repositionElement(target);
 }
+
+
+
+
+// ---- Setup and Unset functions ----
 
 const addDragEvents = (element) => {
     addListener(element, 'enter', toggleDraggingState);
@@ -75,31 +114,48 @@ const removeDragEvents = (element) => {
 };
 
 const setupDraggingContext = () => {
-    document.querySelector(`.${draggingContextClass}`).style.overflow = 'hidden';
+    draggingContext = document.querySelector(`.${draggingContextClass}`);
+    draggingContext.style.overflow = 'hidden';
+    draggingContext.addEventListener('mouseout', abortAllDraggingMotion);
 }
+
+const setupDraggableItems = () => {
+    draggableElements = document.querySelectorAll(`.${draggableClass}`);
+    for (let element of draggableElements) {
+        element.style.cursor = 'pointer';
+        addDragEvents(element);
+        repositionElement(element);
+    }
+}
+
+const unsetDraggableItems = () => {
+    for (let element of draggableElements) {
+        element.style.cursor = 'unset';
+        removeDragEvents(element);
+    }
+}
+
+const unsetDraggingContext = () => {
+    draggingContext.style.overflow = 'unset';
+    draggingContext.removeEventListener('mouseout', abortAllDraggingMotion);
+}
+
+
 
 // -- Callbacks
 
 const afterInsert = () => {
     return new Promise((resolve) => {
         setupDraggingContext();
-
-        //Setup element list declared in module scope
-        draggableElements = document.querySelectorAll('.draggable');
-        for (let element of draggableElements) {
-            addDragEvents(element);
-        }
+        setupDraggableItems();
         resolve();
     });
 }
 
 const beforeRemove = () => {
     return new Promise((resolve) => {
-        console.log('removing draggable');
-        //Read module-level element list
-        for (let element of draggableElements) {
-            removeDragEvents(element);
-        }
+        unsetDraggingContext();
+        unsetDraggableItems();
         resolve();
     });
 }
